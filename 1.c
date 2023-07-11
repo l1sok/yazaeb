d260f1e7c600c62eb2efd0e77125fed499c1fb41
l1sok <halslaaaa@gmail.com>
2023-07-11 15:24:40 +0300

    nemnozhk podredachila

--- 1.c
+++ 1.c
@@ -1,40 +1,40 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <math.h> 
+#define MAXLINE 1000
  
-int main(void) 
-{ 
-    int ch,i=0; char s1 [1000]; char s2[1000]; char s3[1000]; 
-    while ((ch=getchar())!='\n') 
-    { 
-        s1[i++]=ch; 
-    } 
-    s1[i]='\0'; 
-    i=0; 
-    while ((ch=getchar())!='\n') 
-    { 
-        s2[i++]=ch; 
-    } 
-    s2[i]='\0'; 
-    i=0; 
-    while ((ch=getchar())!='\n') 
-    { 
-        s3[i++]=ch; 
-    } 
-    s3[i]='\0'; 
-double a,b,c,d; 
-a=atof(s1); b=atof(s2); c=atof(s3); 
-d=b*b-4*a*c; 
-    if (a) 
-    { 
+void getcoef   (char s []) {
+    int ch;
+    for (int i = 0; (ch = getchar()) != '\n'; i++) {
+        s[i] = ch;
+    }
+}
+
+int main() { 
+    char coef1 [MAXLINE] = ""; getcoef(coef1);
+    char coef2[MAXLINE] = ""; getcoef(coef2);
+    char coef3[MAXLINE] = ""; getcoef(coef3);
+    double coef_a = atof(coef1);
+    double coef_b = atof(coef2);
+    double coef_c = atof(coef3); 
+    double d = pow(coef_b, 2)-4*coef_a*coef_c;
+    if (coef_a) { 
             if (d>0) 
-                printf("2 razlichnyh kornya: %.8g %.8g", (-b-sqrt(d))/(2*a), (-b+sqrt(d))/(2*a)); 
+                printf("2 razlichnyh kornya: %.8g %.8g\n", (-coef_b-sqrt(d))/(2*coef_a), (-coef_b+sqrt(d))/(2*coef_a)); 
             else if (d==0) 
-                printf("1 koren': %.8g",-b/(2*a)); 
-                else 
-                printf("net norm korney"); 
+                printf("1 koren': %.8g\n",-coef_b/(2*coef_a)); 
+            else 
+                printf("net norm korney\n"); 
     } 
-    else 
-    printf("eto ne kvadratnoe"); 
+    else {
+        if (coef_b) {
+            printf("1 koren': %.8g\n", -coef_c/coef_b);
+        }
+        else 
+            if (coef_c)
+                printf("net korney\n");
+            else
+                printf("dohera korney\n");
+    }
     return 0; 
 }
